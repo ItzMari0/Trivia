c4dd442d6ae05ae5fcc244a03e84685e61fa095a
ItzMari0 <msajr@outlook.com>
2022-09-08 17:40:01 -0300

    [GRUPO 10] Requisito 4 resolvido

---
+++ src/tests/helpers/Login.test.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from '../../App';
+import renderWithRouterAndRedux from './renderWithRouterAndRedux';
+
+const INPUT_NAME = 'input-player-name';
+const INPUT_EMAIL = 'input-gravatar-email';
+const NAME = 'tryber';
+const EMAIL = 'tryber@trybe.com';
+const MOCK_FETCH = {
+  "response_code":0,
+  "response_message":"Token Generated Successfully!",
+  "token":"f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6"
+};
+
+describe('Testa a tela de Login', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(MOCK_FETCH)
+    })
+  });
+  it('a tela é renderizada corretamente', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const inputName = screen.getByTestId(INPUT_NAME);
+    expect(inputName).toBeInTheDocument();
+
+    const inputEmail = screen.getByTestId(INPUT_EMAIL);
+    expect(inputName).toBeInTheDocument();
+
+    const playBtn = screen.getByRole('button', { name: /Play/i });
+    expect(playBtn).toBeInTheDocument();
+  });
+
+  it('os campos de "nome" e "email" são preenchidos e funcionais', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const inputName = screen.getByTestId(INPUT_NAME);
+    const inputEmail = screen.getByTestId(INPUT_EMAIL);
+    const playBtn = screen.getByRole('button', { name: /Play/i });
+
+    userEvent.type(inputName, NAME);
+    expect(inputName).toHaveValue(NAME);
+
+    userEvent.type(inputEmail, EMAIL);
+    expect(inputEmail).toHaveValue(EMAIL);
+    expect(playBtn).not.toHaveAttribute('disabled');
+
+    userEvent.click(playBtn);
+    await waitFor(() => expect(history.location.pathname).toBe('/game'));
+
+  });
+});
