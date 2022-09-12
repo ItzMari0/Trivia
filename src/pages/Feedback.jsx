dec53237eaccda374fd8e1eb35a22ad310b62bf6
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-09-12 18:10:04 -0300

    [GRUPO 10] Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
-    const { correctAnswers, score, history } = this.props;
+    const { assertions, score, history } = this.props;
     const limitCorrectionQuestion = 3;
 
     return (
@@ -14,14 +14,14 @@ class Feedback extends Component {
         <section>
           <h1 data-testid="feedback-text">
             {
-              (correctAnswers < limitCorrectionQuestion)
+              (assertions < limitCorrectionQuestion)
                 ? 'Could be better...' : 'Well Done!'
             }
           </h1>
         </section>
         <p>
           { 'Você acertou ' }
-          <span data-testid="feedback-total-question">{ correctAnswers }</span>
+          <span data-testid="feedback-total-question">{ assertions }</span>
           { ' questões!' }
         </p>
         <p>
@@ -56,6 +56,6 @@ export default connect(mapStateToProps)(Feedback);
 
 Feedback.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
-  correctAnswers: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
   score: PropTypes.number.isRequired,
 };
